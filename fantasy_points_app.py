1e87a427ddcc872213377ef57ffa58761e68bc85
liaobailey <47491229+liaobailey@users.noreply.github.com>
2023-12-23 13:22:26 -0500

    Update fantasy_points_app.py

--- fantasy_points_app.py
+++ fantasy_points_app.py
@@ -1,34 +1,19 @@
-import sys
 import streamlit as st
-import pymongo
 import datetime
 import pandas as pd
 import numpy as np
-import os
 
 
 st.set_page_config(layout="wide")
-st.write('load')
+
 
 # Connect to MongoDB
 @st.cache_data
-def get_mongo_db():
-    client = pymongo.MongoClient("mongodb+srv://baileyliao:OvzIZTVsmg6giPis@cluster0.rifrpxv.mongodb.net")
-    st.write('load')
-    db = client.nba
-    collection = db.boxscores
-    return collection
-
+def load_data():
+    df = pd.read_csv('boxscores.csv')
 # Retrieve and display data
-@st.cache_data
-def main():
-    collection = get_mongo_db()
-    st.write('success')
-    data = list(collection.find())
-    df = pd.DataFrame(data)
-    return df
 
-df = main()
+df = load_data()
 
 
 pts_input = st.sidebar.number_input("PTS: ", value = .5)
