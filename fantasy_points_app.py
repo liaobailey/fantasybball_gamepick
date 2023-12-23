15b006f366725fe399f788839d772fea1c4a8e43
liaobailey <47491229+liaobailey@users.noreply.github.com>
2023-12-23 12:56:11 -0500

    Update fantasy_points_app.py

--- fantasy_points_app.py
+++ fantasy_points_app.py
@@ -12,7 +12,7 @@ st.write('load')
 # Connect to MongoDB
 @st.cache_data
 def get_mongo_db():
-    client = pymongo.MongoClient(st.secrets["db_uri"])
+    client = pymongo.MongoClient("mongodb+srv://baileyliao:OvzIZTVsmg6giPis@cluster0.rifrpxv.mongodb.net")
     st.write('load')
     db = client.nba
     collection = db.boxscores
