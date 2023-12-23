caf5f0aeacf983aa517cbc2f98b92e0f51454bd8
liaobailey <47491229+liaobailey@users.noreply.github.com>
2023-12-23 13:28:08 -0500

    Update fantasy_points_app.py

--- fantasy_points_app.py
+++ fantasy_points_app.py
@@ -29,6 +29,7 @@ ft3m_input = st.sidebar.number_input("FG3M: ", value = 1.5)
 orb_input = st.sidebar.number_input("OREB: ", value = 1.6)
 drb_input = st.sidebar.number_input("DREB: ", value = 1.5)
 
+st.write('done inputs')
 df['fantasy_pts'] = (df['PTS']*pts_input + df['AST']*assist_input + df['STL']*steals_input + df['BLK']*blocks_input + df['TOV']*tov_input + df['FGM']*fgm_input + df['FGA']*fga_input + df['FTM']*ftm_input + df['FTA']*fta_input + df['FG3M']*ft3m_input + df['OREB']*orb_input + df['DREB']*drb_input)
 
 df['cal_week'] = df['GAME_DATE'].map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').isocalendar()[1])
