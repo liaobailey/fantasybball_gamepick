6a3e4ba171b061afdb9a26765cc7d7173a3d2cd6
liaobailey <47491229+liaobailey@users.noreply.github.com>
2023-12-23 12:54:54 -0500

    Update fantasy_points_app.py

--- fantasy_points_app.py
+++ fantasy_points_app.py
@@ -6,4 +6,24 @@ import os
 import streamlit as st
 import pymongo
 
-print('hello')
+st.set_page_config(layout="wide")
+st.write('load')
+
+# Connect to MongoDB
+@st.cache_data
+def get_mongo_db():
+    client = pymongo.MongoClient(st.secrets["db_uri"])
+    db = client.nba
+    collection = db.boxscores
+    return collection
+
+# Retrieve and display data
+@st.cache_data
+def main():
+    collection = get_mongo_db()
+    st.write('success')
+    data = list(collection.find())
+    df = pd.DataFrame(data)
+    return df
+
+df = main()
